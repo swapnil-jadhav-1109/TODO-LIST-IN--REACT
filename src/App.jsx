d272414812e5d08163ac757ee78422d245d5a5dc
swapnil-jadhav-1109 <swapniljadhav4912@gmail.com>
2024-06-30 21:03:37 +0530

    Todo List

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,39 @@ import './App.css'
 
 function App() {
   
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim()) {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  const removeTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
 
   return (
     <>
       <h1 className='Heading'>TODO LIST</h1>
 
      <div className='main'>
-     <input type="text" placeholder='Add New Items' />
-     <button>Add</button>
+     <input
+      type="text"
+      value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+       placeholder='Add New Items'
+        />
+     <button className="Btn" onClick={addTask}>Add</button>
+     <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task} <button onClick={() => removeTask(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
      </div>
     </>
   )
